4ffb0139b2d04034887932fdadaecea800af30bc
Anthony rolland <tonyprolland@gmail.com>
2019-01-29 13:46:30 -0500

    Startup file for docker image

---
+++ startup.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+import re
+import subprocess as sp
+
+"""
+Assumes installation on Linux system
+"""
+
+JUPYTER_CONFIG = "~/.jupyter/jupyter_notebook_config.py"
+PORT_NUMBER = 8888
+
+def check_installed(file_name):
+	try:
+		sp.check_output(["which", file_name])
+		return True
+	except sp.CalledProcessError:
+		return False
+
+re_subs = [(re.compile("c.NotebookApp.ip =.*"), "c.NotebookApp.ip = '*'"),
+	(re.compile("c.NotebookApp.open_browser =.*"), "c.NotebookApp.open_browser = False"),
+	(re.compile("c.NotebookApp.port =.*"), "c.NotebookApp.port = {}".format(PORT_NUMBER))]
+
+def edit_jupyter():
+	file_lines = None
+	with open(JUPYTER_CONFIG, "r") as old:
+		file_lines = old.readlines()
+
+	for i in range(len(file_lines)):
+		for regex, substitution in re_subs:
+			file_lines[i] = regex.sub(substitution, file_lines[i])
+
+	with open(JUPYTER_CONFIG, "w") as new_file:
+		new_file.writelines(file_lines)
+
+def launch_notebook():
+	if not check_installed("jupyter"):
+		sp.run(["conda", "install", "jupyter"])
+	try:
+		sp.check_output(["ls", JUPYTER_CONFIG])
+	except sp.CalledProcessError:
+		sp.run(["jupyter", "notebook", "--generate-config"])
+		edit_jupyter()
+		sp.run(["jupyter-notebook", "--no-browser", "--port={}".format(PORT_NUMBER)])
+
+if __name__ == '__main__':
+	print("Is vim installed?", check_installed('vim'))
+	print("Is sl installed?", check_installed("sl"))
