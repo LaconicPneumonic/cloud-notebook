770a2fb6dbdf48a99f4aaa5b05bd24da5d08f720
Anthony rolland <tonyprolland@gmail.com>
2019-01-29 13:46:07 -0500

    Example setup for launching gcloud instance in cloud

---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# Install GCLOUD
+
+echo "Installing gcloud"
+sleep 3
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+# Install DOCKER
+echo "Installing docker "
+sleep 3
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+gcloud compute instances create-with-container nginx-vm \
+    --container-image INSERT_HERE
