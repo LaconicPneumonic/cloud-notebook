1a9dfdb3fef605879e3f39352c21aecfc14bb2a2
Anthony rolland <tonyprolland@gmail.com>
2019-02-13 15:07:20 -0500

    Use prefab Docker Image for VM and Launch from CLI
    This commit makes the following changes:
    1. Switched Jupyter's docker image for tensorflow for simplicity's sake
    2. Got rid of app structure since it is not neccesary
    3. Starts deep learning instance in the cloud at a specific IP address

--- install.sh
+++ install.sh
@@ -1,38 +1,42 @@
 #! /bin/bash
 # Install GCLOUD
 
-# echo "Installing gcloud"
-# sleep 3
-# # Create environment variable for correct distribution
-# export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "Installing gcloud"
+sleep 3
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
-# # Add the Cloud SDK distribution URI as a package source
-# echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
-# # Import the Google Cloud Platform public key
-# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-# # Update the package list and install the Cloud SDK
-# sudo apt-get update && sudo apt-get install google-cloud-sdk
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 
-# # Install DOCKER
-# echo "Installing docker "
-# sleep 3
+# Create an deep learning VM on gcloud
+IMAGE_FAMILY="tf-latest-gpu"
+ZONE="us-west1-b"
+INSTANCE_NAME="cloud-notebook"
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt-get update
-# apt-cache policy docker-ce
-# sudo apt-get install -y docker-ce
+gcloud compute addresses create jupyter-cloudbook \
+	--zone=$ZONE
 
-gcloud compute instances create-with-container mit-bridge-vm \
-	--container-image docker.io/jupyter/tensorflow-notebook  \
-	--custom-memory=128GB \
-	--tags http-server
+gcloud compute instances create $INSTANCE_NAME \
+	--zone=$ZONE \
+	--image-family=$IMAGE_FAMILY \
+	--image-project=deeplearning-platform-release \
+	--boot-disk-size=56GB \
+	--maintenance-policy=TERMINATE \
+	--accelerator="type=nvidia-tesla-v100,count=1" \
+	--metadata="install-nvidia-driver=True" \
+	--tags=http-server \
+	--address=jupyter-cloudbook \
+	--metadata-from-file=startup-script=start_docker.sh
 
-gcloud compute firewall-rules create allow-http \
-    --allow tcp:80 --target-tags http-server
+echo "put the corresponding IP address for 'jupyter-cloudbook' into your browser"
+echo "Your password will be 'temporaryBridge'"
+echo "CHANGE IT IMMEDIATELY"
 
-# gcloud compute addresses create [ADDRESS_NAME] \
-#     [--region [REGION] | --global ] \
-#     [--ip-version [IPV4 | IPV6]]
+gcloud compute addresses list
